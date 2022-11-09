b745e392f1e6026c863c17599c44bb48634b7d12
saadol <saad.ouledlafqui@usmba.ac.ma>
2022-11-09 12:09:36 +0000

    some updates

--- ft_printf.c
+++ ft_printf.c
@@ -2,24 +2,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-
-void ft_putnbr(int nb, int *i)
-{
-    size_t x;
-    x = nb;
-    if (nb < 0)
-    {
-        ft_putchar('-', i);
-        x *= -1;
-    }
-    if (x < 10)
-        ft_putchar(x + 48, i);
-    else
-    {
-        ft_putnbr(x / 10, i);
-        ft_putnbr(x % 10, i);
-    }
-}
 void ft_putchar(char str, int *i)
 {
     write(1, &str, 1);
@@ -37,14 +19,58 @@ void ft_putstr(char *str, int *i)
             t++;
         }
 }
+void ft_putnbr(int nb, int *i)
+{
+    size_t x;
+    x = nb;
+    if (nb < 0)
+    {
+        ft_putchar('-', i);
+        x *= -1;
+    }
+    if (x < 10)
+        ft_putchar(x + 48, i);
+    else
+    {
+        ft_putnbr(x / 10, i);
+        ft_putnbr(x % 10, i);
+    }
+}
+
+
+void x_flag(int ptr, char flag, int *len)
+{
+    if (ptr < 16)
+    {
+        if (flag == 'x') 
+            ft_putchar("0123456789abcdef"[ptr % 16], len);
+        else
+            ft_putchar("0123456789ABCDEF"[ptr % 16], len);
+    }
+    else
+    {
+        x_flag(ptr / 16, flag, len);
+        x_flag(ptr % 16, flag, len);
+    }
+} 
 void checking_flag(va_list arg, char str, int *i)
 {
     if (str == 'c')
         ft_putchar((char)va_arg(arg, int), i);
     else if (str == 's')
-        ft_putstr((char)va_arg(arg, char *), i);
+    {
+        if (!(va_arg(arg, char *)))
+        {
+            write(1,"(null)",6);
+            *i += 6;
+        }
+    }
     else if (str == 'd' || str == 'i')
         ft_putnbr(va_arg(arg, int), i);
+    else if (str == 'x' || str == 'X')
+        x_flag(va_arg(arg, int), str, i);
+    else if (str == '%') 
+        ft_putchar(str,i); 
 
 }
 int	ft_printf(const char *str, ...)
@@ -71,7 +97,8 @@ int	ft_printf(const char *str, ...)
 }
 int main() {
     // Write C code here
-    ft_printf("%d",-22);
+    int fd = ft_printf("%%");
+    printf("%d", fd);
 
     return 0;
 }
